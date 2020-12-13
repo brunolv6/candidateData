39b982143c62510393339f7d1c3bda6bf6ffceca
Bruno Valle <bruno.valle@corebiz.ag>
2020-12-13 20:41:14 -0300

    testing two branchs in vercel

--- pages/components/Teste.js
+++ pages/components/Teste.js
@@ -3,7 +3,7 @@ import styles from './Teste.module.scss';
 function Teste() {
     return (
         <div className={styles.testes}>
-            <p>Home</p>
+            <p>Nova Branch</p>
             <p className={styles.testes__hohe}>LÁ</p>
         </div>
     )
