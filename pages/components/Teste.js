4e4003fbb7f81d4c8c6f5d6fc550f19792db37db
Bruno Valle <bruno.valle@corebiz.ag>
2020-12-13 20:28:05 -0300

    first commit

---
+++ pages/components/Teste.js
@@ -0,0 +1,12 @@
+import styles from './Teste.module.scss';
+
+function Teste() {
+    return (
+        <div className={styles.testes}>
+            <p>Home 4</p>
+            <p className={styles.testes__hohe}>LÁ</p>
+        </div>
+    )
+}
+
+export default Teste;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Teste from './components/Teste';
+
+function Home () {
+    return <div className="teste">
+        <Teste />
+    </div>
+}
+
+export default Home;
